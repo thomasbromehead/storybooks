d30d26dac3c32c34df5a3e5786b80462ea1cbd0e
thomasbromehead <t_bromehead@yahoo.fr>
2018-06-11 15:53:10 +0200

    google OAuth w/o callback uri setup

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+const passport = require('passport');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
+
+//Use Routes
+app.use('/auth', auth)
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+
 
 const port = process.env.PORT || 5000;
 
