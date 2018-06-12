33d10d96fb20bda80b5afc59d3c021d5069d6bfb
thomasbromehead <t_bromehead@yahoo.fr>
2018-06-12 14:21:49 +0200

    google oAuth working, mobile nav with conditional menu

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,38 @@ const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-router.get('/google/callback', //make sure to have \/google
-  passport.authenticate('google', { failureRedirect: '/' }),
- (req, res) => {
-    // Successful authentication, redirect home.
+router.get('/auth/google/callback',
+  passport.authenticate('google'), // complete the authenticate using the google strategy
+  (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
+    if (err.name === 'TokenError') {
+     res.redirect('/auth/google'); // redirect them back to the login page
+     console.log("error happened");
+    } else {
+     // Handle other errors here
+    }
+  },
+  (req, res) => { // On success, redirect back to '/'
+    res.redirect('/');
+  }
+);
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
     res.redirect('/dashboard');
   });
+
+  //Check if user is connected to allow for access to verify route  
+router.get('/verify', (req, res) => {
+  if(req.user){
+    console.log(req.user);
+  } else {
+    console.log('Not Auth');
+  }
+});
+
+router.get('/logout', (req, res) => {
+ req.logout();
+ res.redirect('/');
+});
 
 module.exports = router;
